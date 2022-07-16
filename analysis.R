02b34f8f44f1f66dda733b621f4ca65883325416
Max Zhuravsky <paperlark@yandex.com>
2022-07-16 23:55:03 +0300

    [master] - add link

--- analysis.R
+++ analysis.R
@@ -8,3 +8,8 @@ ggplot(data = mpg) +
 ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy)) +
   facet_wrap(~ cty, nrow = 2)
+
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  geom_smooth(mapping = aes(x = displ, y = hwy))
