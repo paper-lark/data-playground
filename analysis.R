09f12c7595a7fac0fee6bf9708f2796511f66a07
Max Zhuravsky <paperlark@yandex.com>
2022-07-17 17:11:02 +0300

    [main] - add linter and complete data visualisation

--- analysis.R
+++ analysis.R
@@ -1,15 +1,55 @@
 library(tidyverse)
 ?mpg
-mpg
+?diamonds
+
+# 2. Data visualisation
 
 ggplot(data = mpg) +
-  geom_point(mapping = aes(x = displ, y = hwy, color = class))
+  geom_point(mapping = aes(x = displ, y = hwy), position = position_jitter(width = 0))
 
 ggplot(data = mpg) +
-  geom_point(mapping = aes(x = displ, y = hwy)) +
-  facet_wrap(~ cty, nrow = 2)
+  geom_point(mapping = aes(x = displ, y = hwy, color = class)) +
+  facet_wrap(~class, nrow = 2)
 
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point(mapping = aes(color = drv)) +
+  geom_smooth(mapping = aes(linetype = drv), se = FALSE)
+
+
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut))
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, y = after_stat(round(prop * 100)), group = 1))
+
+
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, fill = color), position = "fill")
+
+ggplot(data = diamonds) +
+  stat_summary(
+    mapping = aes(x = cut, y = depth),
+    fun.min = min,
+    fun.max = max,
+    fun = median
+  )
+
+ggplot(data = diamonds) +
+  geom_pointrange(
+    mapping = aes(x = cut, y = depth),
+    stat = "summary",
+    fun.min = min,
+    fun.max = max,
+    fun = median
+  )
 
 ggplot(data = mpg) +
-  geom_point(mapping = aes(x = displ, y = hwy)) +
-  geom_smooth(mapping = aes(x = displ, y = hwy))
+  geom_boxplot(mapping = aes(x = drv, y = hwy, color = class))
+
+ggplot(mpg, aes(x = factor(1), fill = drv)) +
+  geom_bar() +
+  coord_polar(theta = "y")
+
+ggplot(data = mpg, mapping = aes(x = cty, y = hwy)) +
+  geom_point() +
+  geom_abline() +
+  coord_fixed()
--- libraries.R
+++ libraries.R
@@ -1,2 +1,7 @@
 install.packages("tidyverse")
 install.packages(c("nycflights13", "gapminder", "Lahman"))
+
+install.packages(c("styler", "lintr"))
+
+lintr::use_lintr(type = "tidyverse")
+lintr::lint_dir()
